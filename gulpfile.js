7380ff4764ee890463ab5682e122b4dd76d8d9f0
Jay.M.Hu <jay.m.hu@newegg.com>
2016-04-15 16:37:08 +0800

    Project init.

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync').create();
+
+gulp.task('css', _ =>
+  gulp.src('./src/magicstar.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./dist/'))
+);
+
+gulp.task('serve', (done) => {
+  browserSync.init({
+    server: {
+      baseDir: './',
+      directory: true
+    },
+    ghostMode: false,
+    port: 7410
+  });
+  done();
+});
+
+gulp.task('reload', (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('watch', _ => {
+  return gulp.watch([
+    'src/**/*'
+  ], gulp.series('css', 'reload'));
+});
+
+gulp.task('default', gulp.series('css', 'serve', 'watch'));
